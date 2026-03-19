db62c79da57d1d44e55615b52736205805eeca3a
rafael-patronilo <rafael.patronilo@hotmail.com>
2026-03-19 09:13:12 +0000

    some refactoring

--- optuna_utils.py
+++ optuna_utils.py
@@ -1,5 +1,4 @@
 from optuna import Trial
-from collections import OrderedDict
 
 def suggest_partial_permutation(trial : Trial, list_key : str, items : list[str]) -> list:
     result = []
@@ -8,10 +7,15 @@ def suggest_partial_permutation(trial : Trial, list_key : str, items : list[str]
         if include:
             sort_key = trial.suggest_float(f"{list_key}_{item}_sortkey", 0, 1)
             result.append((sort_key, item))
+    # key = lambda x: x[0] to maintain original order on a tie
     result.sort(key=lambda x: x[0])
     return [item for _, item in result]
 
 def suggest_permutation(trial : Trial, items : list[str], key_prefix : str) -> list:
-    sort_keys = [trial.suggest_float(f"{key_prefix}_{i}_sortkey", 0, 1) for i in range(len(items))]
+    sort_keys = [
+        trial.suggest_float(f"{key_prefix}_{item}_sortkey", 0, 1) 
+        for item in items
+    ] 
+    # key = lambda x: x[0] to maintain original order on a tie
     sorted_items = sorted(zip(sort_keys, items), key=lambda x: x[0])
     return [item for _, item in sorted_items]
