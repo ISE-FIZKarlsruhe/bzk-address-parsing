227dbe31dc57473a944d79b891491a5a42cf237f
rafael-patronilo <rafael.patronilo@hotmail.com>
2026-04-01 14:30:00 +0000

    Query is slow (pessimist estimate 2 months on full dataset)

--- geonames_db_search.py
+++ geonames_db_search.py
@@ -14,125 +14,186 @@ import enum
 import dataclasses
 import textwrap
 import warnings
+import textwrap
 
 @dataclasses.dataclass
-class TextMatchingAlgorithmProperties:
-    name : LiteralString
-    return_type : Literal["similarity", "distance"]
-    supports_cutoff : bool = False
+class EntityTypeProperties:
+    entity_type : LiteralString
+    hierarchy_level : int # lower means higher in the hierarchy. Induces a partial order
+    sql_filter : str
 
-class TextMatchingAlgorithm(TextMatchingAlgorithmProperties, enum.Enum):
-    # Text matching algorithms supported by duckdb: https://duckdb.org/docs/stable/sql/functions/text#text-similarity-functions
-    LEVENSHTEIN = "levenshtein", "distance"
-    DAMERAU_LEVENSHTEIN = "damerau_levenshtein", "distance"
-    HAMMING = "hamming", "distance"
-    JACCARD = "jaccard", "similarity"
-    JARO = "jaro_similarity", "similarity", True
-    JARO_WINKLER = "jaro_winkler_similarity", "similarity", True
+class EntityType(EntityTypeProperties, enum.Enum):
+    Country = "Country", 0, "feature_code IN ('TERR', 'PCLI', 'PCL', 'PCLF', 'LTER', 'ZN', 'PCLD', 'PCLH', 'PCLS', 'PRSH', 'PCLIX')"
+    State = "State", 1, "feature_code IN ('ADM1', 'ADM1H', 'ADMDH', 'ADMD')"
+    Region = "Region", 1, ("feature_code IN ('RGN', 'RGNH', 'ADM1', 'ADM1H', 'ADMDH', 'ADMD', 'ADM2', 'ADM2H', 'ADM3H', "
+        "'ADM3', 'ADM4', 'ADM4H', 'ADM5')")
+    District = "District", 2, "feature_code IN ('ADM1', 'ADM1H', 'ADMDH', 'ADMD', 'ADM2', 'ADM2H', 'ADM3H', 'ADM3')"
+    City = "City", 3, "feature_class == 'P'"
+    Neighborhood = "Neighborhood", 4, "feature_class == 'P'"
 
-
-@dataclasses.dataclass
-class NameSearchQuery:
-    table : str
-    filter_feature_codes : bool
-    topk : int = 5
-    threshold : int | float = 3
-    text_matching_algorithm : TextMatchingAlgorithm = TextMatchingAlgorithm.LEVENSHTEIN
-
-    def build_query(self) -> str:
-        # avoid SQL injection by validating type of the parameters before using them to construct the query
-        assert isinstance(self.text_matching_algorithm, TextMatchingAlgorithm)
-        assert isinstance(self.topk, int) and self.topk > 0
-        assert isinstance(self.threshold, (int, float))
-
-        if self.text_matching_algorithm.supports_cutoff:
-            text_match_invocation = f"{self.text_matching_algorithm.name}(key, ?, {self.threshold})"
-        else:
-            text_match_invocation = f"{self.text_matching_algorithm.name}(key, ?)"
-
-        if self.text_matching_algorithm.return_type == "distance":
-            threshold_comparison_operator = "<="
-            order_direction = "ASC"
-        else:
-            threshold_comparison_operator = ">="
-            order_direction = "DESC"
-        
-        if self.filter_feature_codes:
-            feature_code_filter = "AND\n    featureCode IN (?)"
-        else:
-            feature_code_filter = ""
-
-        return textwrap.dedent(f"""
-            SELECT *, {text_match_invocation} AS score
-            FROM geonames
-            WHERE 
-                score {threshold_comparison_operator} {self.threshold} {feature_code_filter}
-            ORDER BY score {order_direction}
-            LIMIT {self.topk}
-        """).strip()
+    def __gt__(self, other):
+        if not isinstance(other, EntityType):
+            return NotImplemented
+        return self.hierarchy_level > other.hierarchy_level
     
-    def __str__(self):
-        return self.build_query(filter_feature_codes=False)
+    def __lt__(self, other):
+        if not isinstance(other, EntityType):
+            return NotImplemented
+        return self.hierarchy_level < other.hierarchy_level
+    
+class GeographicNameProvider(enum.Enum):
+    GEONAMES = "geonames"
+    GND = "gnd"
+    WIKIDATA = "wikidata" # Not used currently
 
-ENTITY_TYPE_SQL_FILTERS = {
-    "Country": "featureCode IN ('TERR', 'PCLI', 'PCL', 'PCLF', 'LTER', 'ZN', 'PCLD', 'PCLH', 'PCLS', 'PRSH', 'PCLIX')",
-    "State": "featureCode IN ('ADM1', 'ADM1H', 'ADMDH', 'ADMD')",
-    "Region": "featureCode IN ('RGN', 'RGNH', 'ADM1', 'ADM1H', 'ADMDH', 'ADMD', 'ADM2', 'ADM2H', 'ADM3H', 'ADM3', 'ADM4', 'ADM4H', 'ADM5')",
-    "District": "featureCode IN ('ADM1', 'ADM1H', 'ADMDH', 'ADMD', 'ADM2', 'ADM2H', 'ADM3H', 'ADM3')",
-    "City" : "featureClass == 'P'"
-}
+@dataclasses.dataclass
+class LinkedAddressPart:
+    part : str
+    matched_name : str
+    entity_type : EntityType
+    feature_class : str
+    feature_code : str
+    geonames_id : int
+    distance : int
+    match_method : Literal["levenshtein", "abbreviation_expansion"]
+    name_provenance : GeographicNameProvider
+    is_preferred : bool
 
-def search_closest_matches_query(
-        entity_type : Optional[str] = None,
+
+@dataclasses.dataclass
+class LinkedAddress:
+    address : str
+    parts : list[LinkedAddressPart]
+    levenshtein_distance : int
+    # neighbor distance between the country that matched the address and the country that contains the corresponding place.
+    # Usually 0, however some addresses might contain cities that changed countries.
+    country_distance : int = 0
+
+def expand_abbreviation_to_pattern(part : str) -> str:
+    """
+    Expands an abbreviation, if it is one, to a pattern that would match the corresponding full name.
+    """
+    # TODO abbreviation size limit is arbitrary
+    if len(part) < 4 and part.isupper():
+        return "".join(f"{char}%" for char in part if char.isalpha())
+    elif "." in part:
+        pattern = part.replace(".", "%")
+        return pattern
+    return part
+
+def build_closest_matches_query(
+        entity_type : Optional[EntityType] = None,
         topk : int = 5,
         threshold : int | float = 3,                       
     ):
-
+    if isinstance(entity_type, str):
+        entity_type = EntityType[entity_type]
+    elif not isinstance(entity_type, EntityType) and entity_type is not None:
+        raise ValueError(f"Invalid entity type: {entity_type}")
+    
     if entity_type is not None:
-        entity_type_filter = ENTITY_TYPE_SQL_FILTERS.get(entity_type)
+        entity_type_filter = entity_type.sql_filter
         if entity_type_filter is None:
             warnings.warn(f"Unknown entity type: {entity_type}. No filter will be applied.")
     else:
         entity_type_filter = None
     if entity_type_filter is not None:
-        entity_type_filter = " AND \n" + entity_type_filter
+        entity_type_filter = entity_type_filter + " AND "
+    else:
+        entity_type_filter = "-- No entity type filter"
     
+    ranking_order = """
+ORDER BY 
+    CASE 
+        WHEN raw_distance = 0 THEN 0 
+        WHEN cleaned_distance = 0 THEN 1
+        WHEN may_be_abbreviation THEN 2 
+        ELSE 3 
+    END,
+    raw_distance, 
+    cleaned_distance,
+    CASE WHEN isPreferredName IS TRUE THEN 0 ELSE 1 END
+ASC
+"""
+
     query = f"""
-        SELECT *, 
-            levenshtein(
-                nfc_normalize(lower(name)), 
-                nfc_normalize(lower(?))
-            ) AS distance
-        FROM allNames
-        WHERE distance <= {threshold} {entity_type_filter}
-        ORDER BY distance ASC
-        LIMIT {topk}
-    """
+WITH 
+ranked_matches AS(
+    SELECT 
+        nfc_normalize(alternateName) AS match_name,
+        nfc_normalize(?) AS query_name,
+        regexp_replace(lower(strip_accents(match_name)), '[^\\w\\s]', '', 'g') AS clean_match_name,
+        regexp_replace(lower(strip_accents(query_name)), '[^\\w\\s]', '', 'g') AS clean_query_name,
+        levenshtein(match_name, query_name) AS raw_distance,
+        levenshtein(clean_match_name, clean_query_name) AS cleaned_distance,
+        CASE 
+            WHEN '.' IN query_name 
+            THEN clean_match_name SIMILAR TO replace(lower(strip_accents(query_name)), '.', '\\w+\\s*')
+            ELSE FALSE 
+        END AS may_be_abbreviation,
+        ROW_NUMBER() OVER (
+            PARTITION BY geonameId 
+{textwrap.indent(ranking_order, ' ' * 4 * 3)}
+            ) AS match_rank,
+        allNames.*, simplifiedGeonames.*, countryInfo.Country
+    FROM allNames NATURAL LEFT JOIN simplifiedGeonames LEFT JOIN countryInfo ON (country_code = ISO)
+    WHERE 
+        (least(raw_distance, cleaned_distance) <= {threshold} OR may_be_abbreviation) AND 
+        {entity_type_filter}
+        (
+            isolanguage IS NULL OR 
+            isolanguage IN countryInfo.Languages OR 
+            split(isolanguage, '-')[1] IN ('en', 'de')
+        )
+),
+ranked_entities AS (
+    SELECT *,
+    RANK() OVER (
+{textwrap.indent(ranking_order, ' ' * 4)}
+    ) AS entity_rank
+    FROM ranked_matches
+    WHERE match_rank = 1
+)
+SELECT * EXCLUDE (match_rank)
+FROM ranked_entities
+WHERE entity_rank <= {topk}
+ORDER BY entity_rank
+"""
+    return query.strip()
+
     
 
 class GeonamesSearch(contextlib.AbstractContextManager):
     def __init__(
             self, 
-            conn : Optional[duckdb.DuckDBPyConnection] = None, 
-            topk : int = 5, 
-            threshold : int = 3,
-            text_matching_algorithm : TextMatchingAlgorithm = TextMatchingAlgorithm.LEVENSHTEIN
+            conn : Optional[duckdb.DuckDBPyConnection] = None,
+            topk : int = 5,
+            threshold : int | float = 3
         ):
+        self.connection = conn or build_geonames_db.open_or_init_duckdb()
         self.topk = topk
         self.threshold = threshold
-        if isinstance(text_matching_algorithm, str):
-            text_matching_algorithm = TextMatchingAlgorithm[text_matching_algorithm]
-        elif not isinstance(text_matching_algorithm, TextMatchingAlgorithm):
-            raise ValueError(f"Invalid text matching algorithm: {text_matching_algorithm}")
-        self.distance_algorithm = text_matching_algorithm
-        self.conn = conn or build_geonames_db.open_or_init_duckdb()
     
-    def search_closest_matches(self, entities: list[tuple[str, str]], include_gnd_names : bool = True):
-        # TODO implement search using duckdb and levenshtein distance
-        pass
-    
+    def find_closest_matches(
+            self,
+        parts : list[str],
+        entity_type : Optional[EntityType] = None,
+        
+    ):
+        query = build_closest_matches_query(entity_type, self.topk, self.threshold)
+        #self.connection.execute("PREPARE closest_matches_query AS\n" + query)
+        results = []
+        for part in parts:
+            matches = self.connection.execute(query, [part]).fetchdf()
+            # query_name will be nfc normalized and may no longer match the original part
+            # hence we add the original part as a separate column
+            matches.insert(0, "query_part", part) 
+            results.append(matches)
+        #self.connection.execute("DEALLOCATE my_stmt")
+        return pd.concat(results, ignore_index=True)
+
     def close(self):
-        self.conn.close()
+        self.connection.close()
 
     def __enter__(self):
         return super().__enter__()
@@ -143,60 +204,3 @@ class GeonamesSearch(contextlib.AbstractContextManager):
     
     def __del__(self):
         self.close()
-
-def _to_feature_codes(key : str) -> set[str]:
-        if key == "Country":
-            return FEATURE_CODE_HIERARCHY[0]
-        elif key == "State":
-            return FEATURE_CODE_HIERARCHY[1]
-        elif key in ["Region", "District"]:
-            # Depending on the country, admin 1 might be a district
-            return {code for level in FEATURE_CODE_HIERARCHY[1:6] for code in level}
-        elif key == "City":
-            return FEATURE_CODE_HIERARCHY[6]
-        elif key == "Neighborhood":
-            return FEATURE_CODE_HIERARCHY[7]
-        else:
-            raise ValueError(f"Unknown feature code key: {key}")
-
-class PlaceLookupTable:
-    def __init__(self, table_path: str | Path):
-        #compile_tables(table_path) TODO
-        self.table = pd.read_csv(table_path, keep_default_na=False)
-        self.search_col = self.table["key"]
-        feature_codes = self.table["featureCode"].unique()
-        for feature_code in feature_codes:
-            assert any(feature_code in level for level in FEATURE_CODE_HIERARCHY), f"Feature code {feature_code} in table is not in the defined hierarchy"
-        self.table_per_level = []
-        for level in FEATURE_CODE_HIERARCHY:
-            self.table_per_level.append(self.table[self.table["featureCode"].isin(level)])
-    
-    
-    
-    def lookup(self, place_name : str, topk : int = 5, max_distance=3, feature_codes=None) -> pd.DataFrame:
-        if isinstance(feature_codes, str):
-            feature_codes = _to_feature_codes(feature_codes)
-        #place_name = normalize_for_search(place_name) TODO 
-        matches_per_level = []
-        for table in reversed(self.table_per_level): # reverse to prioritize higher levels in the hierarchy
-            distances = table["key"].apply(
-                lambda key: utils.levenshtein(place_name, key, case_insensitive=True, max_distance=max_distance)
-                ).nsmallest(topk, keep="all")
-            distances = distances[distances <= max_distance]
-            matches = table.loc[distances.index].copy()
-            matches["distance"] = distances
-            matches_per_level.append(matches)
-        def sort_key(df):
-            if df.empty:
-                return (float('inf'), 1)
-            else:
-                best_match = df.loc[df["distance"].idxmin()]
-                preferred = 0 if best_match["isPreferred"] else 1
-                return (best_match["distance"], preferred)
-        matches_per_level.sort(key=sort_key)
-        matches = pd.concat(matches_per_level)
-        if feature_codes is not None:
-            matches = matches[matches["featureCode"].isin(feature_codes)]
-        return matches
-    
-
